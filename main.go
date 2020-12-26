5dea3a5bcafa33b5b9b12b12c034f83d7c4a29ef
Moritz Gnisia <moritzgnisia@gmail.com>
2020-12-26 09:42:15 +0100

    adding with template for test

--- main.go
+++ main.go
@@ -79,6 +79,32 @@ func psqlConnectionString() string {
 		host, port, user, password, dbname)
 }
 
+type Tile struct {
+	Z    int
+	X    int
+	Y    int
+	Lat  float64
+	Long float64
+}
+
+type Conversion interface {
+	deg2num(t *Tile) (x int, y int)
+	num2deg(t *Tile) (lat float64, long float64)
+}
+
+func (*Tile) Deg2num(t *Tile) (x int, y int) {
+	x = int(math.Floor((t.Long + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
+	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
+	return
+}
+
+func (*Tile) Num2deg(t *Tile) (lat float64, long float64) {
+	n := math.Pi - 2.0*math.Pi*float64(t.Y)/math.Exp2(float64(t.Z))
+	lat = 180.0 / math.Pi * math.Atan(0.5*(math.Exp(n)-math.Exp(-n)))
+	long = float64(t.X)/math.Exp2(float64(t.Z))*360.0 - 180.0
+	return lat, long
+}
+
 func FindMinMax(line orb.LineString) MinMax {
 	minmax := MinMax{90.0, -90.0, 180.0, -180.0}
 	for _, p := range line {
