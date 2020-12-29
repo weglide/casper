f322fe4f0d458752927829a627e2c7b7b849a004
Moritz Gnisia <moritzgnisia@gmail.com>
2020-12-29 11:57:36 +0100

    update bash script for dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 # Simple shell script to install dependencies
 packages=(
-    'github.com/paulmach/orb'
-    'github.com/gorilla/mux'
-    'github.com/gogo/protobuf/proto'
-    'github.com/pkg/errors'
-    'github.com/lib/pq'
-    'github.com/githubnemo/CompileDaemon'
-    'github.com/aws/aws-lambda-go/lambda'
-    'github.com/aws/aws-lambda-go/events'
+    "github.com/fogleman/gg"
+	"github.com/lib/pq"
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/encoding/wkb"
+	"github.com/paulmach/orb/geojson"
 )
 for package in ${packages[@]}; do
     echo "go get ${package}"
