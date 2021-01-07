20e6ba5c1a74aadbed135873cbe28ffc58e1029f
Moritz Gnisia <moritzgnisia@gmail.com>
2021-01-07 12:41:36 +0100

    wrote function for test casess

--- main_test.go
+++ main_test.go
@@ -5,20 +5,19 @@ import (
 	"testing"
 )
 
-func FindTiles(bbox *[4]float64) (Level *Tile, Level2 *Tile) {
-	TileLeft := new(Tile)
-	TileRight := new(Tile)
-	TileLeft.Lat = bbox[0]
-	TileLeft.Long = bbox[2]
-	TileRight.Lat = bbox[1]
-	TileRight.Long = bbox[3]
-	TileRight.Z = 11
-	TileLeft.Z = 11
+type TestCase struct {
+	bbox      [4]float64
+	ZoomLevel int16
+	Name      string
+}
 
+func FindTiles(bbox *[4]float64) (Level *Tile, Level2 *Tile) {
+	TileLeft := Tile{11, 0, 0, bbox[0], bbox[2]}
+	TileRight := Tile{11, 0, 0, bbox[1], bbox[3]}
 	for z := 0; z < 11; z++ {
 		TileLeft.X, TileLeft.Y = TileLeft.Deg2num()
 		TileRight.X, TileRight.Y = TileRight.Deg2num()
-		distanceL := TileLeft.Distance(TileRight)
+		distanceL := TileLeft.Distance(&TileRight)
 		log.Println("----------")
 		log.Println(TileLeft.X, TileLeft.Y, "|", TileRight.X, TileRight.Y)
 		log.Println("DistanceL: ", distanceL, "Zoom", TileLeft.Z)
@@ -29,7 +28,17 @@ func FindTiles(bbox *[4]float64) (Level *Tile, Level2 *Tile) {
 			TileRight.Z--
 		}
 	}
-	return TileLeft, TileRight
+	return &TileLeft, &TileRight
+}
+
+func CheckCase(TestBBox TestCase, t *testing.T) {
+	TileLeft, TileRight := FindTiles(&TestBBox.bbox)
+	if TileLeft.Z != TileRight.Z {
+		t.Errorf("Zoom Levels are not matching, LeftTile %d, RightTile %d", TileLeft.Z, TileRight.Z)
+	}
+	if TileLeft.Z != TestBBox.ZoomLevel {
+		t.Errorf("Expected Zoom Level is wrong: %d", TileLeft.Z)
+	}
 }
 
 func TestNum2deg(t *testing.T) {
@@ -51,37 +60,10 @@ func TestNum2deg(t *testing.T) {
 	*/
 	// Coordinates based on https://www.gps-coordinates.net/
 	// 						Lat Ber  Lat NY    Long Ber    Long NY
-	log.Println("Case Berlin - New York")
-	TestBBox := [4]float64{52.517037, 40.712728, 13.38886, -74.006015}
-	TileLeft, TileRight := FindTiles(&TestBBox)
-	if TileLeft.Z != TileRight.Z {
-		t.Errorf("Zoom Levels are not matching, LeftTile %d, RightTile %d", TileLeft.Z, TileRight.Z)
-	}
-	if TileRight.Z != 2 {
-		t.Errorf("Expected Zoom Level is wrong: %d", TileLeft.Z)
-	}
-	// BBox consist out of Berlin and Hamburg
-	// replacing coordinates of New York with Berlin
-	log.Println("Case Berlin - Hamburg")
-	TestBBox[1] = 53.550341
-	TestBBox[3] = 10.000654
-	TileLeft, TileRight = FindTiles(&TestBBox)
-	if TileLeft.Z != TileRight.Z {
-		t.Errorf("Zoom Levels are not matching, LeftTile %d, RightTile %d", TileLeft.Z, TileRight.Z)
-	}
-	if TileLeft.Z != 7 {
-		t.Errorf("Expected Zoom Level is wrong: %d", TileLeft.Z)
-	}
-	log.Println("Case Berlin - Barcelona")
-	TestBBox[1] = 41.1494512
-	TestBBox[3] = -8.6107884
-	TileLeft, TileRight = FindTiles(&TestBBox)
-	if TileLeft.Z != TileRight.Z {
-		t.Errorf("Zoom Levels are not matching, LeftTile %d, RightTile %d", TileLeft.Z, TileRight.Z)
-	}
-	// expected zoom level should be 4
-	if TileLeft.Z != 4 {
-		t.Errorf("Expected Zoom Level is wrong: %d", TileLeft.Z)
-	}
-
+	CaseBNY := TestCase{[4]float64{52.517037, 40.712728, 13.38886, -74.006015}, 2, "Berlin - New York"}
+	CheckCase(CaseBNY, t)
+	CaseBHAM := TestCase{[4]float64{52.517037, 53.550341, 13.38886, 10.000654}, 7, "Berlin - Hamburg"}
+	CheckCase(CaseBHAM, t)
+	CaseBBARC := TestCase{[4]float64{52.517037, 41.1494512, 13.38886, -8.6107884}, 4, "Berlin - Barcelona"}
+	CheckCase(CaseBBARC, t)
 }
