4941b5b8d01df1eca7f9e92b46b21d0bffd0d4dc
Moritz Gnisia <moritzgnisia@gmail.com>
2021-01-15 10:03:49 +0100

    update functions

--- functions.go
+++ functions.go
@@ -54,9 +54,66 @@ type Image struct {
 	StartIndex int16
 	NoImages   int16
 	Images     map[int16][2]int16
+	bbox       [4]float64
 }
 
-// Download determins the tiles to be downloaded
+// FindTiles returns the tiles tht have a distance of one or two to each other
+func FindTiles(bbox *[4]float64) (Level *Tile, Level2 *Tile) {
+	TileLeft := Tile{11, 0, 0, bbox[1], bbox[0]}
+	TileRight := Tile{11, 0, 0, bbox[3], bbox[2]}
+	for z := 0; z < 11; z++ {
+		TileLeft.X, TileLeft.Y = TileLeft.Deg2num()
+		TileRight.X, TileRight.Y = TileRight.Deg2num()
+		distanceX, distanceY := TileLeft.Distance(&TileRight)
+		if distanceX <= 1 && distanceY <= 1 {
+			break
+		} else if distanceX > 1 || distanceY > 1 {
+			TileLeft.Z--
+			TileRight.Z--
+		}
+	}
+	log.Println(TileLeft.X, TileLeft.Y, "|", TileRight.X, TileRight.Y)
+	return &TileLeft, &TileRight
+}
+
+func (Im *Image) CreateImage() {
+	// WidthHeight maps the tiles ordering to the shift of hight and width
+	var WidthHeight = make(map[int16][2]int)
+	WidthHeight[0] = [2]int{0, 0}
+	WidthHeight[1] = [2]int{0, 1}
+	WidthHeight[2] = [2]int{1, 0}
+	WidthHeight[3] = [2]int{1, 1}
+	TileLeft, TileRight := FindTiles(&Im.bbox)
+	Im, RootKey := TileLeft.Download(TileRight)
+	log.Println("before", RootKey)
+	DownloadTiles(Im, TileLeft.Z)
+	log.Println(RootKey)
+	im, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", Im.Images[RootKey][0], Im.Images[RootKey][1]))
+	if err != nil {
+		panic(err)
+	}
+	w := im.Bounds().Size().X
+	h := im.Bounds().Size().Y
+	fmt.Println("Creating new image with", int(Im.NoImages))
+	dc := gg.NewContext(w*int(Im.NoImages), h*int(Im.NoImages))
+	dc.DrawImage(im, WidthHeight[RootKey][1]*w, WidthHeight[RootKey][0]*h)
+	// dc.DrawCircle(p.Lon()*512+10, (1-p.Lat())*512, 1.0)
+	dc.SavePNG("images/merged_1.png")
+	for k, value := range Im.Images {
+		if k != RootKey {
+			log.Println("Loading", value)
+			im, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", value[0], value[1]))
+			if err != nil {
+				panic(err)
+			}
+			dc.DrawImage(im, WidthHeight[k][1]*w, WidthHeight[k][0]*h)
+		}
+	}
+	dc.SavePNG("images/merged.png")
+
+}
+
+// Download determines the tiles to be downloaded
 func (t *Tile) Download(ref *Tile) (Im *Image, RootKey int16) {
 	Im = new(Image)
 	distanceX, distanceY := t.Distance(ref)
