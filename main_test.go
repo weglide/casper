3f4d07b90cd558bb3f9cf065d2ade0da2465c326
Moritz Gnisia <moritzgnisia@gmail.com>
2021-02-27 10:15:16 +0100

    creating separate function for plotting image

--- main_test.go
+++ main_test.go
@@ -69,6 +69,42 @@ func CheckImages(ImageName string) {
 	}
 }
 
+func CreateImage(tiles map[int64][2]int16) {
+	ImageComposed, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", tiles[0][0], tiles[0][1]))
+	if err != nil {
+		panic(err)
+	}
+
+	// Width and Height of Image
+	w, h := ImageComposed.Bounds().Size().X, ImageComposed.Bounds().Size().Y
+	// Standard Case two images
+	dc := gg.NewContext(w*int(4), h*int(4))
+
+	// Drawing context with 4 images -> 2 Images per Direction
+	// Draw Image top left corner
+	dc.DrawImage(ImageComposed, 0, 0)
+	CounterWidth := 0
+	CounterHeight := 0
+	// for k, value := range tiles {
+	for k := 0; k < 16; k++ {
+		log.Println("Loading", k)
+		im, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", tiles[int64(k)][0], tiles[int64(k)][1]))
+		log.Println(tiles[int64(k)][0], tiles[int64(k)][1])
+		if err != nil {
+			panic(err)
+		}
+		// log.Println("Shift", CounterWidth*w, CounterHeight*h)
+		dc.DrawImage(im, CounterWidth*w, CounterHeight*h)
+		CounterHeight++
+		if (k+1)%4 == 0 && k >= 1 {
+			// log.Println("shifting")
+			CounterWidth++
+			CounterHeight = 0
+		}
+	}
+	dc.SavePNG(fmt.Sprintf("images/%s_merged.png", "16x16"))
+}
+
 func TestFindTiles(t *testing.T) {
 
 	// BBox consist out of Berlin and New York
@@ -101,46 +137,16 @@ func TestFindTiles(t *testing.T) {
 	if ImageBNY.RootTile.Y != 0 {
 		t.Errorf("Roottile Y is not equal to 0")
 	}
-	log.Println("Zoom Level", ImageBNY.RootTile.Z)
-
+	if ImageBNY.RootTile.Z != 0 {
+		t.Errorf("Zoom Level Z is not equal to 0")
+	}
 	tiles, ZoomIncrease := TilesDownload(ImageBNY.RootTile.X, ImageBNY.RootTile.Y, ImageBNY.RootTile.Z)
-	log.Println(tiles)
+
 	// Download Tiles with Zoom Level
 	DownloadTiles(tiles, ImageBNY.RootTile.Z+ZoomIncrease)
+	CreateImage(tiles)
 
 	// Load the image for the top left corner
-	ImageComposed, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", tiles[0][0], tiles[0][1]))
-	if err != nil {
-		panic(err)
-	}
-	// Width and Height of Image
-	w, h := ImageComposed.Bounds().Size().X, ImageComposed.Bounds().Size().Y
-	// Standard Case two images
-	dc := gg.NewContext(w*int(4), h*int(4))
-
-	// Drawing context with 4 images -> 2 Images per Direction
-	// Draw Image top left corner
-	dc.DrawImage(ImageComposed, 0, 0)
-	CounterWidth := 0
-	CounterHeight := 0
-	// for k, value := range tiles {
-	for k := 0; k < 16; k++ {
-		log.Println("Loading", k)
-		im, err := gg.LoadJPG(fmt.Sprintf("images/%d_%d.jpeg", tiles[int64(k)][0], tiles[int64(k)][1]))
-		log.Println(tiles[int64(k)][0], tiles[int64(k)][1])
-		if err != nil {
-			panic(err)
-		}
-		// log.Println("Shift", CounterWidth*w, CounterHeight*h)
-		dc.DrawImage(im, CounterWidth*w, CounterHeight*h)
-		CounterHeight++
-		if (k+1)%4 == 0 && k >= 1 {
-			// log.Println("shifting")
-			CounterWidth++
-			CounterHeight = 0
-		}
-	}
-	dc.SavePNG(fmt.Sprintf("images/%s_merged.png", "16x16"))
 
 	// log.Println("Tiles Downloaded", TilesDownload(0, 0, 0))
 	// log.Println(ImageBNY.Tiles[0].TilesDownload())
@@ -174,8 +180,19 @@ func TestFindTiles(t *testing.T) {
 	// CheckImages("BerlinRio_merged")
 	// ImageBRIO.DrawImage(&CaseBRIO.bbox, "BerlinRio")
 
-	// CaseBHAM := TestCase{[4]float64{10.000654, 52.517037, 13.38886, 53.550341}, 7, "Berlin - Hamburg"}
-	// ImageBHAM := NewImage(CaseBHAM.bbox)
+	CaseBHAM := TestCase{[4]float64{10.000654, 52.517037, 13.38886, 53.550341}, 7, "Berlin - Hamburg"}
+	ImageBHAM := NewImage(CaseBHAM.bbox)
+	ImageBHAM.FindTiles()
+	if ImageBHAM.RootTile.X != 8 {
+		t.Errorf("Roottile X is not equal to 0, the current values is %d", ImageBHAM.RootTile.X)
+	}
+	if ImageBHAM.RootTile.Y != 5 {
+		t.Errorf("Roottile Y is not equal to 0, the current values is %d", ImageBHAM.RootTile.Y)
+	}
+	if ImageBHAM.RootTile.Z != 4 {
+		t.Errorf("Zoom Level Z is not equal to 0, the current values is %d", ImageBHAM.RootTile.Z)
+	}
+	// tiles, ZoomIncrease := TilesDownload(ImageBHAM.RootTile.X, ImageBHAM.RootTile.Y, ImageBHAM.RootTile.Z)
 	// // Find Tiles including the zoom level
 	// ImageBHAM.FindTiles()
 	// ImageBHAM.CheckZoomLevel(4, t)
